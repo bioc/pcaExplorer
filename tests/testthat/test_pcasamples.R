d7314c281b9ae9e4d41f010acc1aedcf9deab42e
Federico Marini <marinif@uni-mainz.de>
2024-09-25 19:09:24 +0200

    working a bit more on the pcaplot test suite

--- tests/testthat/test_pcasamples.R
+++ tests/testthat/test_pcasamples.R
@@ -9,6 +9,21 @@ test_that("Checks on the pca on the samples", {
   p <- pcaplot(rlt_multifac, intgroup = c("condition", "tissue"))
   expect_true(is(p, "gg"))
   
+  expect_message({
+    p_def <- pcaplot(rlt_multifac)
+    expect_true(is(p_def, "gg"))
+  }, "Defaulting to 'condition'")
+  
+  expect_error({
+    rlt_nocoldata <- rlt_multifac
+    colData(rlt_nocoldata)$condition <- NULL
+    colData(rlt_nocoldata)$tissue <- NULL
+    colData(rlt_nocoldata)$sizeFactor <- NULL
+    
+    pcaplot(rlt_nocoldata)
+  },
+  "No colData has been provided")
+  
   dat <- pcaplot(rlt_multifac, intgroup = c("condition", "tissue"), returnData = TRUE)
   
   expect_error(pcaplot(rlt_multifac, intgroup = "foo"))
